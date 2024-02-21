437ca53c83b8465e05bd1833d7181073247a785b
coolalmaz <coolalmaz001@gmail.com>
2024-02-21 13:20:58 +0300

    папрарапра

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log(123);
+//const result = (confirm("Are you here?"));
+//console.log(result);
+
+/*const answer = +prompt("How old are you?", "18"); //+ перед prompt для того чтобы тип данных был числом, а не строкой
+console.log(answer);*/
+
+const answers = [];
+answers[0] = prompt("Как Вас зовут?", "");
+answers[1] = prompt("Какой у Вас никнейм?", "");
+answers[2] = +prompt("Сколько Вам лет?", "");
+
+
+
+if (answers[2] < 18) {
+    alert( "Иди нахуй, школотрон! АХАХАХАХ");
+    close();
+}
+else 
+    alert(`Добро пожаловать, ${answers[0]}!`);
