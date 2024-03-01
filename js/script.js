951af401a4fbb76cd0f9ca1b809f7987d0654180
coolalmaz <coolalmaz001@gmail.com>
2024-03-01 16:06:51 +0300

    add cycle

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@ answers[0] = prompt("Как Вас зовут?", "");
 answers[1] = prompt("Какой у Вас никнейм?", "");
 answers[2] = +prompt("Сколько Вам лет?", "");
 
-if (answers[2] < 18) {
+if (answers[2] < 18 || answers[2] == null || answers[2] == '') {
     alert( "Иди нахуй, школотрон! АХАХАХАХ");
     close();
 }
 
 else {
     alert(`Добро пожаловать, ${answers[0]}!`);
-    const numberOfGames = +prompt("Сколько игр вы уже прошли?", "");
+    const numberOfGames = +prompt("Сколько игр за последнее время Вы прошли полностью?", "");
 
     const personalGameDB = {
         count : numberOfGames,
@@ -25,12 +25,19 @@ else {
         privat : false
     };
 
-    const game1 = prompt("Назовите одну из последних пройденных видеоигр:", ""),
-          rate1 = prompt("На сколько вы её оцените по 10-ти бальной шкале?", ""),
-          game2 = prompt("Назовите одну из последних пройденных видеоигр:", ""),
-          rate2 = prompt("На сколько вы её оцените по 10-ти бальной шкале?", "");
+    if (numberOfGames != 0){
 
-    personalGameDB.games[game1] = rate1;
-    personalGameDB.games[game2] = rate2;
+        for (let i = 1; i <= numberOfGames; i++){
+            const game = prompt("Назовите одну из последних пройденных видеоигр:", ""),
+                  rate = +prompt("На сколько вы её оцените по 10-ти бальной шкале?", "");
+            if (rate != null && rate != '' && rate <= 10 && game != null && game != '' && game.length < 51){
+                personalGameDB.games[game] = rate;
+            }
+            else{
+                i--;
+            }
+        }
+    }
+
     console.log(personalGameDB);
 }
